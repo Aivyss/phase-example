f7061ffe0e6ec1ca4ee68f8e38760dd31aedb7ed
Aivyss <hklee6417@gmail.com>
2024-12-09 17:00:54 +0900

    phase 3 (I/F implementation) phase 3-1: EmailService実装

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module phase_example
 go 1.23
 
 require (
-	github.com/labstack/echo/v4 v4.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sesv2 v1.38.4
+	github.com/labstack/echo/v4 v4.13.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.32.6 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.25 // indirect
+	github.com/aws/smithy-go v1.22.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- service/email.go
+++ service/email.go
@@ -2,10 +2,61 @@ package service
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"phase_example/dto"
 )
 
 type MailingService interface {
 	// Send メールを送信する
 	Send(ctx context.Context, info dto.EmailInfo) error
+}
+
+type mailingService struct {
+	client *sesv2.Client
+}
+
+// Send メールを送信する
+func (s *mailingService) Send(ctx context.Context, info dto.EmailInfo) error {
+	content := &types.Content{
+		Data: &info.Body,
+	}
+	var body *types.Body
+	switch info.Type {
+	case dto.HTML:
+		body = &types.Body{
+			Html: content,
+		}
+	case dto.TEXT:
+		body = &types.Body{
+			Text: content,
+		}
+	}
+
+	input := &sesv2.SendEmailInput{
+		FromEmailAddress: &info.From,
+		Destination: &types.Destination{
+			ToAddresses:  info.To,
+			BccAddresses: info.Bcc,
+			CcAddresses:  info.Cc,
+		},
+		Content: &types.EmailContent{
+			Simple: &types.Message{
+				Body: body,
+				Subject: &types.Content{
+					Data: &info.Subject,
+				},
+			},
+		},
+	}
+
+	_, err := s.client.SendEmail(ctx, input)
+	return err
+}
+
+// NewMailingService　service.MailingServiceを変換 (AWS SES-v2の実装)
+func NewMailingService(client *sesv2.Client) MailingService {
+	return &mailingService{
+		client: client,
+	}
 }
