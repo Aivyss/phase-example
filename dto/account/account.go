3c768d1d46dafe7fbd711e03c9ed94672cfdbdd4
Aivyss <hklee6417@gmail.com>
2024-12-09 16:58:42 +0900

    phase 3 (I/F implementation) phase 3-3: AccountRepository実装

---
+++ dto/account/account.go
@@ -0,0 +1,8 @@
+package account
+
+type Account struct {
+	ID         int
+	UserID     string
+	Password   string
+	IsVerified bool
+}
--- repository/account.go
+++ repository/account.go
@@ -1,5 +1,50 @@
 package repository
 
+import (
+	"errors"
+	"fmt"
+	"phase_example/dto/account"
+	"sync"
+)
+
 type AccountRepository interface {
 	Insert(userID string, password string) error
+}
+
+type accountRepository struct {
+	sequence int
+	mutex    sync.Mutex
+	memory   map[int]account.Account
+	idSet    map[string]bool
+}
+
+func (a *accountRepository) Insert(userID string, password string) error {
+	a.mutex.Lock()
+	if a.idSet[userID] {
+		a.mutex.Unlock()
+		return errors.New("duplicated user id")
+	}
+
+	accnt := account.Account{
+		ID:         a.sequence,
+		UserID:     userID,
+		Password:   password,
+		IsVerified: false,
+	}
+	a.memory[accnt.ID] = accnt
+	a.idSet[accnt.UserID] = true
+	a.sequence++
+	a.mutex.Unlock()
+
+	return nil
+}
+
+func NewAccountRepository() AccountRepository {
+	fmt.Println("autowired: AccountRepository")
+
+	return &accountRepository{
+		sequence: 0,
+		memory:   map[int]account.Account{},
+		idSet:    map[string]bool{},
+	}
 }
